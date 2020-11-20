cc5b3fb1a9ff16267a8115a7ee79406cfb7bebb2
Luis bernal <bernikiller@hotmail.com>
2020-11-19 20:46:49 -0500

    servicio 70%

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NoticiasI } from '../interfaces/noticias-i';
 import { SectionsI } from '../interfaces/sections-i';
 import { NoticiasService } from '../services/noticias.service';
 
@@ -12,10 +13,14 @@ import { NoticiasService } from '../services/noticias.service';
 export class HomePage implements OnInit {
   
   section:Array<SectionsI> = [];
+  noticia:Array<NoticiasI> = [];
 
 
   constructor(public noticiasService:NoticiasService) { 
-    this.section = this.noticiasService.getNoticias();
+    this.section = this.noticiasService.getSection();
+    this.noticia = this.noticiasService.getNoticias();
+    console.log(this.noticia);
+    console.log(this.section);
   }
 
   ngOnInit() {
--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,18 +1,29 @@
-import { Injectable } from '@angular/core';
-import { SectionsI } from '../interfaces/sections-i';
-import db from '../db/sections.json';
+import { Injectable } from "@angular/core";
+import { SectionsI } from "../interfaces/sections-i";
+import db from "../db/sections.json";
+import { NoticiasI } from "../interfaces/noticias-i";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NoticiasService {
+  constructor() {}
 
-  constructor(){}
-
-  getNoticias(){
+  getSection() {
     const sections: Array<SectionsI> = db;
     return sections;
-   
-}
-  
+  }
 
+  getNoticias() {
+    const noticias: Array<SectionsI> = db;
+    let matches: Array<NoticiasI> = [];
+    let found;
+    noticias.forEach((section) => {
+      found = section.noticia;
+      //console.log(found);
+      if (found.length > 0) {
+        matches.push(found);
+      }
+    });
+    return matches;
+  }
 }
