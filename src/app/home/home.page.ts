48eaf376219e804af4f5d2ecf1e61e55570c3b28
Luis bernal <bernikiller@hotmail.com>
2020-11-26 17:52:03 -0500

    100%

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,34 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { CategoriasI } from '../interfaces/categorias-i';
-import { NoticiasI } from '../interfaces/noticias-i';
-import { SectionsI } from '../interfaces/sections-i';
-import { NoticiasService } from '../services/noticias.service';
-
-
+import { Component, OnInit } from "@angular/core";
+import { CategoriasI } from "../interfaces/categorias-i";
+import { NoticiasI } from "../interfaces/noticias-i";
+import { SectionsI } from "../interfaces/sections-i";
+import { NoticiasService } from "../services/noticias.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  selector: "app-home",
+  templateUrl: "./home.page.html",
+  styleUrls: ["./home.page.scss"],
 })
 export class HomePage implements OnInit {
-  
-  section:Array<SectionsI> = [];
-  noticia:Array<NoticiasI> = [];
-  buscartexto = '';
+  section: Array<SectionsI> = [];
+  noticia: Array<NoticiasI> = [];
+  buscartexto = "";
 
-
-  constructor(public noticiasService:NoticiasService) { 
+  constructor(public noticiasService: NoticiasService) {
     this.section = this.noticiasService.getSection();
-
-   
   }
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
+  buscar(event) {
+    if (event.detail.value != "") {
+      this.buscartexto = event.detail.value;
+    } else {
+      this.buscartexto = "";
+    }
+    const items = Array.from(document.querySelectorAll("ion-grid"));
+    const items2 = Array.from(document.querySelectorAll(".body"));
+    let style;
+    requestAnimationFrame(() => {
+      items.forEach((section) => {
+        let arr:any=[];
+        let number=0;
+        let showldshow = false;
+        section.querySelectorAll(".body").forEach((noticias) => {
+          noticias.querySelectorAll("ion-label").forEach((categoria) => {
+            
+               arr.push(categoria.textContent.toLowerCase().indexOf(this.buscartexto) > -1);
+          });
+        });
+        arr.forEach(e => {
+          if(e===true)number++;
+        });
+        if(number>0){
+          showldshow = true;
+        }
+        if (!showldshow) {
+          style = "display: none";
+        } else {
+          style = "";
+        }
+        section.setAttribute("style", style);
+      });
 
-  buscar(event){
-    this.buscartexto = event.detail.value;
+      items2.forEach((noticias) => {
+        let arr:any=[];
+        let number=0;
+        let showldshow = false;
+        noticias.querySelectorAll("ion-label").forEach((categoria) => {
+          
+             arr.push(categoria.textContent.toLowerCase().indexOf(this.buscartexto) > -1);
+        });
+        arr.forEach(e => {
+          if(e===true)number++;
+        });
+        if(number>0){
+          showldshow = true;
+        }
+        if (!showldshow) {
+          style = "display: none";
+        } else {
+          style = "";
+        }
+        noticias.setAttribute("style", style);
+      });
+    });
   }
 }
--- src/app/pipes/filtro.pipe.ts
+++ src/app/pipes/filtro.pipe.ts
@@ -9,8 +9,20 @@ export class FiltroPipe implements PipeTransform {
       return arreglo;
     }
     texto = texto.toLowerCase();
+    let arr = arreglo.forEach(section => {
+      let arr2 = section.noticia.forEach(noticia => {
+        let arr3 = noticia.categoria.filter(categoria =>{
+          let arr4 = categoria.nombre.toLowerCase().includes(texto);
+          return arr4;
+        });
+        return arr3;
+      });
+      return arr2;
+    });
+    /*
     return arreglo.filter((item) => {
       return item.nombre.toLowerCase().includes(texto);
     });
+    */
   }
 }
